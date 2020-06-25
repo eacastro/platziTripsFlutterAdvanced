a786419c4418f5a2cf0a69969ea96fc91c4021fa
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-25 16:29:10 -0500

    Creation of buttons bar

---
+++ lib/button_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'circle_button.dart';
+
+class ButtonsBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 0.0,
+        vertical: 10.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
