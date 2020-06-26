7b6249bdf96a7fbd75c076a95b8ace47e1ab763a
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-25 19:57:01 -0500

    Creating ProfileTrips widget

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:platzi_trips/profile_background.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
