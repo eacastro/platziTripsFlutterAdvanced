dceccc84ff06b0ce6129dffe660ce5ef7773eb32
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-25 19:31:20 -0500

    Creating custom bottom navigation bar

--- lib/platzi_trips_cupertino.dart
+++ lib/platzi_trips_cupertino.dart
@@ -1,10 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PlatziTripsCupertino extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.indigo,
+              ),
+              title: Text(''),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.indigo,
+              ),
+              title: Text(''),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.indigo,
+              ),
+              title: Text(''),
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (_) => HomeTrips(),
+              );
+              break;
+
+            case 1:
+              return CupertinoTabView(
+                builder: (_) => SearchTrips(),
+              );
+              break;
+
+            case 2:
+              return CupertinoTabView(
+                builder: (_) => ProfileTrips(),
+              );
+              break;
+          }
+        },
+      ),
     );
   }
 }
