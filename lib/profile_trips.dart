d9f712912587d31d93b31ca26de8b5d4a1ffc9e3
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-25 20:12:44 -0500

    Creating class Place

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:platzi_trips/profile_background.dart';
 import 'package:platzi_trips/profile_header.dart';
+import 'package:platzi_trips/profile_places_list.dart';
 
 class ProfileTrips extends StatelessWidget {
   @override
