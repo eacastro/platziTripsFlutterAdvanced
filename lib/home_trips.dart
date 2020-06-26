dbbe119e5e423f3b4fc26e9cee4bac9e9e174089
Edisson Castro <edissoncastro.11@hotmail.com>
2020-06-25 19:22:00 -0500

    Modifying main execution point of the app

--- lib/home_trips.dart
+++ lib/home_trips.dart
@@ -1,35 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:platzi_trips/description_place.dart';
-import 'package:platzi_trips/header_appbar.dart';
-import 'package:platzi_trips/review_list.dart';
 
-class HomeTrips extends StatelessWidget {
-  String descriptionDummy = 'Lorem Ipsum is simply dummy text' +
-      'of the printing and typesetting industry. Lorem Ipsum has' +
-      'been the industry\'s standard dummy text ever since the 1500s' +
-      ', when an unknown printer took a galley of type and scrambled ' +
-      'it to make a type specimen book. \n\nLorem Ipsum is simply ' +
-      'dummy text of the printing and typesetting industry.' +
-      'Lorem Ipsum has been the industry\'s standard dummy text' +
-      ' ever since the 1500s, when an unknown printer took a galley' +
-      'of type and scrambled it to make a type specimen book.';
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ListView(
-          children: <Widget>[
-            DescriptionPlace(
-              'Bahamas',
-              4,
-              descriptionDummy,
-            ),
-            ReviewList(),
-          ],
-        ),
-        HeaderAppBar(),
-      ],
+    return MaterialApp(
+      title: 'Platzi Trips',
+      theme: ThemeData(),
+      home: PlatziTripsCupertino(),
     );
   }
 }
